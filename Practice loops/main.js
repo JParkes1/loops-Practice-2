10bb976741fe123a248d4e25793cb665c09827d8
Joe Parkes <52543866+JParkes1@users.noreply.github.com>
2019-07-22 09:19:59 -0600

    Add files via upload

---
+++ Practice loops/main.js
@@ -0,0 +1,34 @@
+// JavaScript Loops
+
+// DOM Elements
+let outputEl = document.getElementById('output');
+
+// // Example 1
+// for (let num = 1; num <= 500; num++) {
+//     console.log('I’m so happy!')
+// }
+
+// // Example 2
+// for (let num = 12; num <= 800; num += 4) {
+//     console.log(num);
+// }
+
+// // Example 3
+// for (let num = 55; num >= 11; num -= 2) {
+//     console.log(num);
+// }
+
+
+// Example 4
+// let sum = 0;
+//     for ( let num = 5; num <= 50; num++) {
+//     sum += num;
+// }
+// console.log(sum)
+
+// Example 5
+// let sum = 0;
+//     for ( let num = 10; num <= 100; num += 10) {
+//     sum += num;
+// }
+// console.log(sum)
